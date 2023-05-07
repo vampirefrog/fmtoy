3fe698148bb5b783298acde0773390c0fcbdf5e6
vampi <vampi@vampi.tech>
2023-05-07 22:01:27 +0300

    opm file operator order fix

--- opm_file.c
+++ opm_file.c
@@ -66,7 +66,7 @@ ALL_STATES
 				fprintf(stderr, "Operator %c%c parameters %d != 11\n", paramName[0], paramName[1], paramNum); \
 				return -1; \
 			} \
-			int opNum = paramName[0] == 'M' ? (paramName[1] == '1' ? 0 : 2) : (paramName[1] == '1' ? 1 : 3); \
+			int opNum = paramName[0] == 'M' ? (paramName[1] == '1' ? 0 : 1) : (paramName[1] == '1' ? 2 : 3); \
 			opm->voices[curVoice].operators[opNum].ar = params[0]; \
 			opm->voices[curVoice].operators[opNum].d1r = params[1]; \
 			opm->voices[curVoice].operators[opNum].d2r = params[2]; \
